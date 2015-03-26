1091e3d41708977665ef167bd8ac50af3d747da7
queejie <dqj@pls3rdlearning.com>
2015-03-26 12:45:40 -0400

    Ongoing development.

--- backend/lib/Question.php
+++ backend/lib/Question.php
@@ -9,4 +9,23 @@
 namespace App;
 
 class Question extends Model {
+
+	public function map($dbRecord) {
+		$jsonRecord = [];
+		try {
+			$columnNames = array_keys(iterator_to_array($dbRecord));
+			for ($i = 0; $i < count($columnNames); $i++) {
+				$colName = $columnNames[$i];
+				if (empty($this->mapExcludes) || !in_array($colName, $this->mapExcludes)) {
+					$jsonName = $this->colNameToJsonName($colName);
+					$jsonRecord[$jsonName] = iconv('UTF-8', 'UTF-8//IGNORE', utf8_encode($dbRecord[$colName]));
+				}
+			}
+		}
+		catch (\Exception $exception) {
+			var_dump($exception);
+		}
+		return $jsonRecord;
+	}
+
 }
