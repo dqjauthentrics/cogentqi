766f2017c4f7291906091805da6fffdfab87eca6
queejie <dqj@pls3rdlearning.com>
2015-03-26 11:05:19 -0400

    Ongoing development.

--- backend/www/index.php
+++ backend/www/index.php
@@ -1,41 +1,16 @@
 <?php
 namespace App;
-$parts = explode("/", $_SERVER["REQUEST_URI"]);
-$object = @ucfirst($parts[2]);
-if (strstr($object, "XDEBUG")) {
-	$object = NULL;
-}
-$objectNS = !empty($object) ? "App\\" . $object : NULL;
-$objPath = !empty($object) ? "../lib/$object.php" : NULL;
-
 require_once "../lib/Api.php";
-require_once "../lib/Model.php";
-require_once "../lib/Authentication.php";
-if (!empty($objPath)) {
-	if (!@file_exists($objPath)) {
-		echo "Invalid invocation ($objPath)";
-		exit();
-	}
-}
-require_once $objPath;
 
 $dsn = "mysql:dbname=cogentqi_v1_target;host=localhost";
 $username = "cogentqiapp";
 $password = "cogentqi42app";
 
 $api = new Api($dsn, $username, $password, ["MODE" => "development", "TEMPLATES.PATH" => "./templates"]);
-$auth = new Authentication($api);
-$user = $auth->check();
-if (!empty($user)) {
-	$api->get("/", function () use ($api) {
-		$api->sendResult("Welcome to the API.");
-	});
-	$api->get("/test", function () use ($api) {
-		$api->sendResult("TEST");
-	});
-
-	if (strlen($objectNS) > 0) {
-		new $objectNS($api);
-	}
-}
+$api->get("/", function () use ($api) {
+	$api->sendResult("Welcome to the API.");
+});
+$api->get("/test", function () use ($api) {
+	$api->sendResult("TEST");
+});
 $api->run();
