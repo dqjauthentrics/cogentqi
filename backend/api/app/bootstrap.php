864e67e87fa79b59b74eb0c08b4777a541c5f6c0
queejie <dqj@pls3rdlearning.com>
2015-10-20 00:13:21 -0400

    addition of Nette and Restful

--- backend/api/app/bootstrap.php
+++ backend/api/app/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__ . '/../vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 $server = @$_SERVER["SERVER_NAME"];
 $parts = @explode(".", $server);
 $infix = @$parts[0];
@@ -11,9 +11,9 @@ $isAjax = strpos($_SERVER["HTTP_ACCEPT"], "application/json") !== FALSE;
 $configurator = new Nette\Configurator;
 if (!$isAjax) {
 	$configurator->setDebugMode(!Nette\Environment::isProduction());
-	$configurator->enableDebugger(__DIR__ . '/../log');
+	$configurator->enableDebugger(dirname(__DIR__) . '/log');
 }
-$configurator->setTempDirectory(__DIR__ . '/../temp');
+$configurator->setTempDirectory(dirname(__DIR__) . '/temp');
 $configurator->createRobotLoader()->addDirectory(__DIR__)->register();
 $configurator->addConfig(__DIR__ . '/config/config.neon');
 $configurator->addConfig(__DIR__ . "/config/config.$infix.neon");
