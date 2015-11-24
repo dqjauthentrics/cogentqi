f6012f1eb921815e87734e05513e530a487f0fcf
amazatron <thrackle912>
2015-11-24 13:51:40 -0500

    more recommendations work

--- backend/sql/updates/0000-Initial.sql
+++ backend/sql/updates/0000-Initial.sql
@@ -7,5 +7,5 @@ CREATE TABLE _db_update (
 UPDATE plan_item SET plan_item_status_id = (SELECT id FROM plan_item_status ORDER BY RAND() LIMIT 1);
 UPDATE plan_item SET score = RandomInt(0,5);
 
-DELETE FROM _db_update WHERE script_name LIKE '0001%';
-INSERT INTO _db_update (script_name) VALUES ('0001-Initial.sql');
+DELETE FROM _db_update WHERE script_name LIKE '0000%';
+INSERT INTO _db_update (script_name) VALUES ('0000-Initial.sql');
