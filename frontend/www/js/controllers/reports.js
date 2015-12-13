2d5924b6bc5d8c3fd79bb070209736602c20d50b
queejie <dqj@pls3rdlearning.com>
2015-12-13 17:15:34 -0500

    Recommendation final fixes.

--- frontend/www/js/controllers/reports.js
+++ frontend/www/js/controllers/reports.js
@@ -30,54 +30,64 @@ angular.module('ReportsControllers', [])
 		function ($rootScope, $scope, $stateParams, Utility) {
 			$scope.data = {
 				resourceAnalysisCfg: {
+
 					title: {
-						text: 'Resources vs Competency Coverage',
-						x: -20 //center
-					},
-					subtitle: {
-						text: 'Source: WorldClimate.com',
-						x: -20
+						text: 'Resource Competency Coverage Analysis'
 					},
 					xAxis: {
-						categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+						categories: ['Assessment', 'Prescription Management', 'Communication', 'Collaboration', 'Medical Knowledge']
 					},
 					yAxis: {
+						min: 0,
 						title: {
-							text: 'Temperature (°C)'
+							text: 'Alignment Level'
 						},
-						plotLines: [
-							{
-								value: 0,
-								width: 1,
-								color: '#808080'
+						stackLabels: {
+							enabled: false
+						}
+					},
+					options: {
+						chart: {
+							type: 'column'
+						},
+						legend: {
+						},
+						tooltip: {
+							formatter: function () {
+								return '<b>' + this.x + '</b><br/>' +
+									this.series.name + ': ' + this.y + '<br/>' +
+									'Total: ' + this.point.stackTotal;
 							}
-						]
-					},
-					tooltip: {
-						valueSuffix: '°C'
-					},
-					legend: {
-						layout: 'vertical',
-						align: 'right',
-						verticalAlign: 'middle',
-						borderWidth: 0
+						},
+						plotOptions: {
+							column: {
+								stacking: 'normal',
+								dataLabels: {
+									enabled: false
+								}
+							}
+						}
 					},
 					series: [
 						{
-							name: 'Tokyo',
-							data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+							name: 'Identifying Allergies',
+							data: [5, 3, 4, 7, 2]
 						},
 						{
-							name: 'New York',
-							data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+							name: 'Patient Communication 101',
+							data: [2, 2, 3, 2, 1]
 						},
 						{
-							name: 'Berlin',
-							data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+							name: 'Collaboration',
+							data: [3, 4, 4, 2, 5]
 						},
 						{
-							name: 'London',
-							data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+							name: 'Medical Terminology',
+							data: [2, 2, 3, 2, 1]
+						},
+						{
+							name: 'Team Communication',
+							data: [2, 2, 3, 2, 1]
 						}
 					]
 				}
