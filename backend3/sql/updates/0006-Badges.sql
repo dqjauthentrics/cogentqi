85c0530f58673a372acc1e502eca2f268b9eff30
queejie <dqj@pls3rdlearning.com>
2015-12-16 17:09:49 -0500

    Badges.

--- backend3/sql/updates/0006-Badges.sql
+++ backend3/sql/updates/0006-Badges.sql
@@ -9,11 +9,21 @@ CREATE TABLE badge (
   module_id   INT             NULL,
   FOREIGN KEY (module_id) REFERENCES module (id)
 );
-INSERT INTO badge (name, description, image, issuer)
-VALUES ('Member Mebook.eu', 'This badge is awarded to members in a Mebook Club.', 'http://create.openbadges.it/images/badge/3',
-        'http://factory.openbadges.it/public/systems/openbadges/issuers/mebook');
-ALTER TABLE member_badge ADD badge_id INT NULL;
+ALTER TABLE member_badge ADD COLUMN badge_id INT NULL;
 ALTER TABLE member_badge ADD CONSTRAINT member_badge_badge__fk FOREIGN KEY (badge_id) REFERENCES badge (id);
+UPDATE member_badge SET badge_id=NULL;
+DELETE FROM badge;
+INSERT INTO badge (name, description, image, issuer) VALUES ('CSS3', 'CSS3 mastery module.', '/site/default/badges/css3.png', 'CogentQI.com');
+INSERT INTO badge (name, description, image, issuer) VALUES ('CSS3', 'CSS3 mastery module.', '/site/default/badges/cup.png', 'CogentQI.com');
+INSERT INTO badge (name, description, image, issuer) VALUES ('CSS3', 'CSS3 mastery module.', '/site/default/badges/gavel.png', 'CogentQI.com');
+INSERT INTO badge (name, description, image, issuer) VALUES ('CSS3', 'CSS3 mastery module.', '/site/default/badges/ipv6.png', 'CogentQI.com');
+INSERT INTO badge (name, description, image, issuer) VALUES ('CSS3', 'CSS3 mastery module.', '/site/default/badges/participation.png', 'CogentQI.com');
+INSERT INTO badge (name, description, image, issuer) VALUES ('CSS3', 'CSS3 mastery module.', '/site/default/badges/prescriptions.png', 'CogentQI.com');
+INSERT INTO badge (name, description, image, issuer) VALUES ('CSS3', 'CSS3 mastery module.', '/site/default/badges/security.png', 'CogentQI.com');
+INSERT INTO badge (name, description, image, issuer) VALUES ('CSS3', 'CSS3 mastery module.', '/site/default/badges/technology.png', 'CogentQI.com');
+INSERT INTO badge (name, description, image, issuer) VALUES ('CSS3', 'CSS3 mastery module.', '/site/default/badges/telegraph.png', 'CogentQI.com');
+INSERT INTO badge (name, description, image, issuer) VALUES ('CSS3', 'CSS3 mastery module.', '/site/default/badges/wisdom.png', 'CogentQI.com');
+UPDATE member_badge SET badge_id=(SELECT id FROM badge ORDER BY RAND() LIMIT 1);
 
 DELETE FROM _db_update WHERE script_name LIKE '0006%';
 INSERT INTO _db_update (script_name) VALUES ('0006-Badges.sql');
