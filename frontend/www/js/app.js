1df98b65cc4ecb67ce8594a21db278a7df66b320
queejie <dqj@pls3rdlearning.com>
2015-12-16 22:54:12 -0500

    Fixes.

--- frontend/www/js/app.js
+++ frontend/www/js/app.js
@@ -118,7 +118,7 @@ angular.module(
 
 			$rootScope.$on('$idleTimeout', function() {
 				// end their session and redirect to login
-				alert('timeout');
+				//alert('timeout');
 			});
 
 			/** Hide the accessory bar by default (remove this to show the accessory bar above the keyboard for form inputs)
