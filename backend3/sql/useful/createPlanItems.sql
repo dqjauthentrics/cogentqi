98a6cacce20c526ebacede55907d8ee477189ec9
queejie <dqj@pls3rdlearning.com>
2015-12-16 22:52:35 -0500

    Fixes.

--- backend3/sql/useful/createPlanItems.sql
+++ backend3/sql/useful/createPlanItems.sql
@@ -5,5 +5,6 @@ INSERT INTO plan_item (module_id, plan_item_status_id, member_id,status_stamp,ra
 
 UPDATE plan_item SET plan_item_status_id = 'C' WHERE mod(id, 4) = 0;
 UPDATE plan_item SET plan_item_status_id = 'E' WHERE mod(id, 5) = 0;
+UPDATE plan_item SET plan_item_status_id = 'R' WHERE mod(id, 3) = 0;
 UPDATE plan_item SET status_stamp = DATE_SUB(current_timestamp,INTERVAL RandomInt(1,30) DAY) WHERE plan_item_status_id='E';
 UPDATE plan_item SET status_stamp = DATE_SUB(current_timestamp,INTERVAL RandomInt(1,350) DAY) WHERE plan_item_status_id='C';
