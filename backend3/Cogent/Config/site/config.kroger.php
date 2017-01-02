8d6778bd5f2bca1f33a30dcf610993bf7203035f
queejie <dqj@authentrics.com>
2017-01-02 18:40:48 +0000

    kroger added

---
+++ backend3/Cogent/Config/site/config.kroger.php
@@ -0,0 +1,8 @@
+<?php
+return [
+	'name'     => 'Kroger Pharmacy',
+	'logo'     => '<img src="/site/kroger/images/logoHeader.png" alt=""/>',
+	'database' => [
+		'dbname' => 'cogentqi_v1_pharmacy'
+	],
+];
